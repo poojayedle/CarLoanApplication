0c8729c44d6a4303b9820047af905a979586da0d
poojayedle <poojamyedle@gmail.com>
2023-07-15 17:54:20 +0530

    error

--- DashBoardforApplication/src/app/entry/login/login.component.ts
+++ DashBoardforApplication/src/app/entry/login/login.component.ts
@@ -13,33 +13,7 @@ export interface user {
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
-  // constructor(private fb:FormBuilder,private router:Router){}
-  // loginForm!: FormGroup;
-  // ngOnInit(): void {
-  //   this.loginForm=this.fb.group({
-  //     userName:[],
-  //     password:[]
-  //   })
-  // }
-  // onLogin()
-  // {
-  //      if(this.loginForm.controls['userName'].value=='re'
-  //      &&this.loginForm.controls['password'].value=='re@123')
-  //      {                          //KEY       VALUE
-  //          sessionStorage.setItem('userType','re');
-
-  //         this.router.navigateByUrl('/dash/re')
-  //      }
-  //      else if(this.loginForm.controls['userName'].value=='oe'
-  //      &&this.loginForm.controls['password'].value=='oe@123')
-  //      {
-  //       sessionStorage.setItem('userType','oe')
-  //       this.router.navigateByUrl('/dash/oe');
-  //      }
-  //      else{
-  //       alert("Enter valid username or password..!")
-  //      }
-  // }
+ 
 
   u: user = {
     username: "",
